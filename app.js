7085304e341e2364215f084f8b2266f06eab354b
edwincaceres24 <edwin.caceresilva@gmail.com>
2023-01-22 22:41:32 -0600

    Adding new functions to search input and box

--- app.js
+++ app.js
@@ -2,22 +2,28 @@ const puppeteer = require('puppeteer');
 
 const runBrowser = async () => {
 
-  const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({headless:false});
   const page = await browser.newPage();
+	let productInfo;
 
-  await page.goto('https://listado.mercadolibre.com.pe/');
+  await page.goto('https://www.mercadolibre.com.pe/');
 
 	try{
-		const h1 = await page.evaluate(() => {
-   	 return document.querySelector('h1').innerText})
-  	console.log(h1);
+		
+		const searchBox = await page.type('.nav-search-input', 'Iphone')
+		const searchButton = await page.click('.nav-icon-search')
+		await page.waitForSelector('.ui-search-result__content-wrapper')
+		
+		const products = await page.$$eval('.ui-search-result__content-wrapper', elements => {
+			console.log(elements.lenght)
+			return elements.map(ele=>console.log(ele.textContent))
+		})
+
+
 	}catch(err){
 		console.log('You have an error:')
 		console.error(err)
 	}
-  
-
-	await browser.close();
 }
 
 runBrowser()
